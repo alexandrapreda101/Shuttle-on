ef18112637349970fcc577fc8baba088b79bc8b5
your_usernamealexandrapreda101 <predaalexandra18@stud.ase.ro>
2020-12-07 18:55:54 +0200

    Adaugat formular

--- frontend/src/components-ai/pages/Formular.js
+++ frontend/src/components-ai/pages/Formular.js
@@ -1,6 +1,38 @@
 import React from 'react';
 import '../../App.css';
+import './Formular.css';
+
 
 export default function Formular() {
-  return <h1 className='formular'>Formular</h1>;
+
+  // transport = ["Metrou","Tramvai","Autobuz","Troleibuz"];
+
+  return (
+    <div className='formular'>
+      <h1>Formular</h1>
+      <div className='inputText'>
+        <label>Punct plecare:</label>
+        <input type='text' name='punct_plecare' />
+        <label>Punct sosire:</label>
+        <input type='text' name='punct_sosire' />
+        <label>Mijloc de transport:</label>
+        <input type='text' name='mijloc_transport' />
+        <label>Numarul:</label>
+        <input type='text' name='numarul' />
+        <label>Ora plecarii:</label>
+        <input type='text' name='ora_plecarii' />
+        <label>Durata calatoriei:</label>
+        <input type='text' name='durata_calatoriei' />
+        <label>Grad aglomerare:</label>
+        <input type='text' name='grad_aglomerare' />
+        <label>Nivel satisfactie:</label>
+        <input type='text' name='nivel_satisfactie' />
+        <label>Alte comentarii:</label>
+        <input type='text' name='alte_comentarii' />
+
+        <button>Submit</button>
+      </div>
+    </div>
+
+  );
 }
