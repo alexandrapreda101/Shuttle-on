31411b84979397f7e7ac83043fa00f7fd2d14c89
your_usernamealexandrapreda101 <predaalexandra18@stud.ase.ro>
2020-12-16 14:43:37 +0200

    Autentificarea si baza de date

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,15 @@ const { response, request } = require('express');
 const express = require('express');
 const app = express();
 const Sequelize = require('sequelize');
+const cors = require("cors")
+const mysql = require("mysql2")
 
-const sequelize = new Sequelize('purple_puffs', 'root', 'pass', {
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+const session = require("express-session")
+
+
+const sequelize = new Sequelize('purple_puffs', 'root', 'password', {
     dialect: "mysql",
     host: "localhost"
 });
@@ -26,6 +33,16 @@ const Recenzie = sequelize.define('review', {
     nivel_satisfactie: Sequelize.STRING,
     alte_comentarii: Sequelize.TEXT
 });
+
+const User = sequelize.define('user', {
+    username: Sequelize.STRING,
+    password: Sequelize.STRING,
+    email: Sequelize.STRING,
+});
+
+Recenzie.belongsTo(User);
+User.hasMany(Recenzie);
+  
 
 app.use('/', express.static('frontend'));
 
@@ -50,62 +67,148 @@ app.post('/formular', (request, response) => {
 });
 
 app.get('/formular', (request, response) => {
-    Recenzie.findAll().then((results)=>{
+    Recenzie.findAll().then((results) => {
         response.status(200).json(results);
     })
 });
 
 app.get('/formular/:id', (request, response) => {
-    Recenzie.findByPk(request.params.id).then((result)=>{
-        if(result) {
+    Recenzie.findByPk(request.params.id).then((result) => {
+        if (result) {
             response.status(200).json(result);
         }
         else {
             response.status(404).send('Resursa nu a fost gasita');
         }
-    }).catch((err)=>{
+    }).catch((err) => {
         console.log(err);
         response.status(500).send('Eroare la baza de date');
     })
 });
 
 app.put('/formular/:id', (request, response) => {
-    Recenzie.findByPk(request.params.id).then((recenzie)=>{
-        if(recenzie){
-            recenzie.update(request.body).then((result)=>{
+    Recenzie.findByPk(request.params.id).then((recenzie) => {
+        if (recenzie) {
+            recenzie.update(request.body).then((result) => {
                 response.status(201).json(result)
-            }).catch((err)=>{
+            }).catch((err) => {
                 console.log(err);
                 response.status(500).send("Eroare la baza de date");
             })
         }
-        else{
+        else {
             response.status(404).send('Resursa nu a fost gasita')
         }
-    }).catch((err)=>{
+    }).catch((err) => {
         console.log(err);
         response.status(500).send("Eroare la baza de date");
     })
 });
 
 app.delete('/formular/:id', (request, response) => {
-    Recenzie.findByPk(request.params.id).then((recenzie)=>{
-        if(recenzie){
-            recenzie.destroy().then((result)=>{
+    Recenzie.findByPk(request.params.id).then((recenzie) => {
+        if (recenzie) {
+            recenzie.destroy().then((result) => {
                 response.status(204).send()
-            }).catch((err)=>{
+            }).catch((err) => {
                 console.log(err);
                 response.status(500).send("Eroare la baza de date");
             })
         }
-        else{
+        else {
             response.status(404).send('Resursa nu a fost gasita')
         }
-    }).catch((err)=>{
+    }).catch((err) => {
         console.log(err);
         response.status(500).send("Eroare la baza de date");
     })
 });
 
-app.listen(8080);
+
+////AUTENTIFICARE////
+
+app.use(express.json())
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true
+}))
+
+
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(session({
+    key: "userId",
+    secret: "proiect_purple_puffs",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 60 * 60 * 24,
+        httpOnly: false,
+    },
+}))
+
+
+
+app.post('/inregistrare', (req, res) => {
+
+    const username = req.body.username
+    const email = req.body.email
+    const password = req.body.password
+
+    const entry = User.create({ username: username, email: email, password: password },
+        (err, result) => {
+            console.log(err);
+        })
+
+
+})
+
+app.get("/autentificare", (req, res) => {
+    if (req.session.user) {
+        res.send({ loggedIn: true, user: req.session.user })
+    } else {
+        res.send({ loggedIn: false })
+    }
+})
+
+
+app.post('/autentificare', (req, res) => {
+
+    const username = req.body.username
+    const password = req.body.password
+
+
+
+    User.findOne({ where: { username: username, password: password } })
+        .then((result) => {
+            if (result) {
+                req.session.user = result
+                console.log(req.session.user)
+                res.status(200).send(result)
+            } else {
+                res.status(401).send({ message: "Parola sau username-ul este gresit!" })
+            }
+
+        }).catch(err => {
+            res.status(500).send({
+                message: "ERROR"
+            });
+        });
+
+})
+
+app.get('/logout', (req, res) => {
+    if (req.session.user) {
+        res.clearCookie('userId');
+        res.send({ loggedIn: false })
+        req.session.destroy()
+    }
+})
+
+
+app.listen(3001, () => {
+    console.log("Server pornit");
+})
 
