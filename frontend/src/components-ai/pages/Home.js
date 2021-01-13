22e5427ddc07b799a1b8a34bad30ae5ea23318df
your_usernamealexandrapreda101 <predaalexandra18@stud.ase.ro>
2021-01-13 20:53:59 +0200

    Recenzii

--- frontend/src/components-ai/pages/Home.js
+++ frontend/src/components-ai/pages/Home.js
@@ -15,7 +15,7 @@ function Home() {
     }, [])
 
     return (
-        <div className='home'>
+        <div class='home'>
             <div className='herosection'>
                 <HeroSection />
             </div>
@@ -23,13 +23,15 @@ function Home() {
             <div className='tabs'>
                 <Tabs />
             </div> */}
+            <h2 class ="recenzieTitlu">Recenzii</h2>
             {listaRecenzii.map((val) => {
                 return (
-                    <div className = "recenzie">Punct plecare:{val.punct_plecare} | Punct sosire: {val.punct_sosire} 
-                    | Mijloc transport: {val. mijloc_transport}| Numarul: {val.numarul}
-                    | Ora plecarii: {val.ora_plecarii}| Durata calatoriei: {val.durata_calatoriei} 
-                    | Grad aglomerare:{val.grad_aglomerare}| Nivel satisfactie: {val.nivel_satisfactie}
-                    | Alte comentarii: {val.alte_comentarii} |User: {val.user.username}</div>
+                    <div className = "recenzie">
+                    Punct plecare : {val.punct_plecare} | Punct sosire : {val.punct_sosire} 
+                     |  Mijloc transport: {val. mijloc_transport}| Numarul: {val.numarul}
+                     |  Ora plecarii: {val.ora_plecarii}| Durata calatoriei: {val.durata_calatoriei} 
+                     |  Grad aglomerare: {val.grad_aglomerare}| Nivel satisfactie: {val.nivel_satisfactie}
+                     |  Alte comentarii:  {val.alte_comentarii} |User: {val.user.username}</div>
                 )
             })}
         </div>
