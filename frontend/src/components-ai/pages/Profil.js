1d4a2400cf93bbfd92321c949aa7304b62cee555
alexandraivanovici <ivanoviciioana18@stud.ase.ro>
2021-01-13 14:41:38 +0200

    Profil

---
+++ frontend/src/components-ai/pages/Profil.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { useHistory } from "react-router-dom";
+import '../../App.css';
+import './Profil.css';
+import Axios from 'axios';
+
+
+export default function Profil() {
+
+  return (
+    <div className='profil'>
+      <h1>Profil</h1>
+      <h1></h1>
+    </div>
+
+  );
+}
